df633af8f69e63c20f2039d987dfe8bfdaf45df0
RandomCyberCoder <emmanueldelgado74@gmail.com>
2024-03-20 12:21:49 -0700

    list

--- c/list.c
+++ c/list.c
@@ -7,7 +7,17 @@ List *lstcreate(){
     list->head = NULL;
 }
 
-void *lstinsert()
+/*return 0 on success and -1 on a failure
+  position 0 = to the first position in the list
+  position list->size means to insert after last element in the list
+  position list-size -1 means to insert before the last element in the list*/
+int lstinsert(List *list, void *val, int position){
+    if(list == NULL) return -1;
+    else if(list->size < position) return -1;
+    else if(position == list->size){
+        if(list->size)
+    }
+}
 
 /* List *lstcreate();
 void lstdestroy(List *);
--- c/list.h
+++ c/list.h
@@ -3,11 +3,12 @@
 typedef struct List{
     int size;
     Node *head;
+    Node *tail;
 } List;
 
 List *lstcreate();
 void lstdestroy(List *);
-void *lstget(List *, char *);
-void lstinsert(List *, char *, void *);
+int lstinsert(List *, void *, int);
 void *lstremove(List *, char*);
+void *lstget(List *, int);
 void lstIsEmpty(List *);
